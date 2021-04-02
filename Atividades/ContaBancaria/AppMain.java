7d7d6304683e355186069247af350c422bd4ff0f
Matheus Santana <matheusanjos_santana@hotmail.com>
2021-04-02 13:44:51 -0300

    Encapsulando e adicionando o constructor

--- Atividades/ContaBancaria/AppMain.java
+++ Atividades/ContaBancaria/AppMain.java
@@ -7,16 +7,12 @@ public class AppMain {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    ContaBancaria conta = new ContaBancaria();
-    conta.nomeBanco = "Inter";
-    conta.numeroAgencia = "0015";
-    conta.numeroConta = "168574";
-    conta.nomeTitular = "Robenilton Junior";
-    conta.tipoConta = 'C';
-    conta.saldo = 0;
-    String continuar = new String();
+    ContaBancaria conta = new ContaBancaria("Inter","0015","168574","Robenilton Junior");
+    conta.setTipoConta('C');
+    String continuar;
+    
     do{
-      System.out.println("Qual a operação?\n Depósito (0)\n Saque (1)\n Ver Saldo (2)");
+      System.out.println("Qual a operação?\n ::Depósito (0)\n ::Saque (1)\n ::Saldo (2)\n ::Extrato (3)\n ::Consultar Dados (4)");
       int operacao = sc.nextInt();
       switch(operacao){
         case 0:{
@@ -31,11 +27,13 @@ public class AppMain {
           conta.Sacar(saque);
           break;
         }
-        case 2:{System.out.printf("Saldo atual: %.2f\n",conta.saldo); break;}
+        case 2:{System.out.printf("Saldo atual: %.2f\n",conta.getSaldo()); break;}
+        case 3: {conta.Extrato(); break;}
+        case 4:{conta.Dados();break;}
+        default: {System.out.println("Opção Inválida");break;}
       }
       System.out.print("Deseja fazer outra operação? (Sim/Nao) ");
       continuar = sc.next();
     }while(continuar.equalsIgnoreCase("Sim"));
-    conta.imprimirDados(conta);
   }
 }
--- Atividades/ContaBancaria/ContaBancaria.java
+++ Atividades/ContaBancaria/ContaBancaria.java
@@ -1,23 +1,107 @@
 package ContaBancaria;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ContaBancaria {
-  String nomeBanco;
-  String numeroAgencia;
-  String numeroConta;
-  String nomeTitular;
-  char tipoConta;
-  double saldo;
-  double Sacar(double valor){
-    if(saldo <= 0 || saldo < valor){
-      System.out.println("Saldo insuficiente!");
-    }else{
-      saldo -= valor;
-    }
+  
+  private Map<String,String> Extrato = new HashMap<>();
+  private String nomeBanco;
+  private String numeroAgencia;
+  private String numeroConta;
+  private String nomeTitular;
+  private char tipoConta;
+  private double saldo;
+  
+  public ContaBancaria(String nomeBanco, String numeroAgencia, String numeroConta, String nomeTitular){
+    this.nomeBanco = nomeBanco;
+    this.numeroAgencia = numeroAgencia;
+    this.numeroConta = numeroConta;
+    this.nomeTitular = nomeTitular;
+    this.saldo = 0.0f;
+  }
+  
+  public String getNumeroAgencia(){
+    return numeroAgencia;
+  }
+  
+  public String getNumeroConta(){
+    return numeroConta;
+  }
+  
+  public String getNomeTitular(){
+    return nomeTitular;
+  }
+  
+  public Double getSaldo(){
     return saldo;
   }
-  double Depositar(double valor){
-    return saldo += valor;
+  
+  public char getTipoConta(){
+    return tipoConta;
   }
-  void imprimirDados(ContaBancaria conta){
-    System.out.printf("Banco: %s | Agência: %s | Conta: %s\n",conta.nomeBanco, conta.numeroAgencia, conta.numeroConta);           System.out.printf("Cliente: %s | Tipo da Conta: %c | Saldo: %.2f\n",conta.nomeTitular, conta.tipoConta, conta.saldo);
+  
+  public char setTipoConta(char tipoConta){
+    if(tipoConta == 'C' || tipoConta == 'P'){
+      this.tipoConta = tipoConta;
+    }else{
+      System.out.println("Tipo de conta inválido");
+    }
+    return this.tipoConta;
+  }
+  
+  public String DataTransacao(){
+    Date dataTransacao;
+    DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+    return dateFormat.format(dataTransacao = new Date());
+  }
+  
+  public double Sacar(double saldo){
+    if(this.saldo <= 0 || this.saldo < saldo){
+      System.out.println("Saldo insuficiente!");
+    }else{
+      this.saldo -= saldo;
+      Extrato.put(DataTransacao(), (" Saque de "+saldo));
+    }
+    return this.saldo;
+  }
+  
+  public double Depositar(double saldo){
+    if(saldo <= 0){
+      System.out.println("Valor inválido");
+    }else{
+      this.saldo += saldo;
+      Extrato.put(DataTransacao(), (" Depósito de "+saldo));
+    }
+    return this.saldo;
+  }
+  
+  public void Extrato(){
+    if(Extrato.isEmpty()){
+      System.out.println("Sem dados para o extrato por período");
+    }else{
+      System.out.println("::::::::::::::DATA = TRANSAÇÃO::::::::::::::");
+      for(Map.Entry<String,String> extrato: Extrato.entrySet()){
+        System.out.println("::::: "+extrato);
+      }
+      System.out.println("::::::::::::::::::::::::::::::::::::::::::::");
+    }
+  }
+  
+  public void Dados(){
+    System.out.printf("Banco: %s\n"
+                    + "Agência: %s\n"
+                    + "Conta: %s\n"
+                    + "Cliente: %s\n"
+                    + "Tipo da Conta: %c\n"
+                    + "Saldo: %.2f\n",
+                      nomeBanco,
+                      getNumeroAgencia(),
+                      getNumeroConta(),
+                      getNomeTitular(),
+                      getTipoConta(),
+                      getSaldo());
   }
 }
