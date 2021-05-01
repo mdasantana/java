b2737a4e70002514f1087ce9d524d31542146944
Matheus Santana <matheusanjos_santana@hotmail.com>
2021-05-01 11:29:48 -0300

    Ajustes e inclusao do getId e getTipoContato

--- Atividades/AgendaBD/Contato.java
+++ Atividades/AgendaBD/Contato.java
@@ -4,20 +4,33 @@ package AgendaBD;
  * @author Matheus Santana
  */
 public class Contato {
-
+  
+  private int id;
   private String nome;
   private String telefone;
   private String email;
+  private String tipoContato;
   
-  Contato(String nome, String telefone, String email){
+  Contato(int id, String nome, String telefone, String email, String tipoContato){
+    this.id = id;
     this.nome = nome;
     this.telefone = telefone;
     this.email = email;
+    this.tipoContato = tipoContato;
+  }
+  Contato(String nome, String telefone, String email, String tipoContato){
+    this.nome = nome;
+    this.telefone = telefone;
+    this.email = email;
+    this.tipoContato = tipoContato;
   }
   Contato(){
     
   }
 
+  public int getId(){
+    return id;
+  }
   
   public String getNome() {
     return nome;
@@ -31,4 +44,7 @@ public class Contato {
     return email;
   }
   
+  public String getTipoContato(){
+    return tipoContato;
+  }
 }
